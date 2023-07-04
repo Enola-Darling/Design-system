f5ae4491a86494a4bb782b6eba323912fdd210be
enola-darling <luna.raimondo@gmail.com>
2023-07-04 18:45:22 +0200

    add responsive grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,65 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import "./App.scss";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-       <p>I'm learning Saas</p>
+    <div className="container">
+      <div className="row-g">
+        <div className="col-1 ">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
       </div>
-     
-    </>
-  )
+      <div className="row-g">
+        <div className="col-2">col2</div>
+        <div className="col-2">col2</div>
+        <div className="col-2">col2</div>
+        <div className="col-2">col2</div>
+        <div className="col-2">col2</div>
+        <div className="col-2">col2</div>
+      </div>
+      <div className="row-g">
+        <div className="col-3">col3</div>
+        <div className="col-3">col3</div>
+        <div className="col-3">col3</div>
+        <div className="col-3">col3</div>
+      </div>
+      <div className="row-g">
+        <div className="col-4">col4</div>
+        <div className="col-4">col4</div>
+        <div className="col-4">col4</div>
+      </div>
+      <div className="row-g">
+        <div className="col-5">col5</div>
+        <div className="col-7">col7</div>
+      </div>
+      <div className="row-g">
+        <div className="col-12">col12</div>
+      </div>
+      <div className="row-g">
+        <div className="col-3">col3</div>
+        <div className="col-3">col3</div>
+        <div className="col-6">col6</div>
+      </div>
+      <div className="row-g">
+        <div className="col-1">col1</div>
+        <div className="col-1">col1</div>
+      </div>
+      <div className="row-g">
+        <div className="col-4">col4</div>
+        <div className="col-8">col8</div>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
